42ed2a90d8eaf3783241a6f8632fd8005da934b1
“MahaMG” <dev.mahamg@gmail.com>
2023-12-13 06:21:24 +0300

    first commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '732457',
+        paymentStatus: 'Due',
+        shipmentStatus: 'Pending',
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '87652',
+        paymentStatus: 'Refunded',
+        shipmentStatus: 'Decliend',
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '23398',
+        paymentStatus: 'Due',
+        shipmentStatus: 'Pending',
+    },
+    {
+        productName: 'Drone with camera Drone',
+        productNumber: '289361',
+        paymentStatus: 'Paid',
+        shipmentStatus: 'Delivered',
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '92736',
+        paymentStatus: 'Paid',
+        shipmentStatus: 'Delivered',
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '923696',
+        paymentStatus: 'Due',
+        shipmentStatus: 'Pending',
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '23279',
+        paymentStatus: 'Paid',
+        shipmentStatus: 'Delivered',
+    }
+]
